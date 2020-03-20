66d3384accec63556c1691095deb7c439da571f4
mykhailopylka <mykhailo.pylka@gmail.com>
2020-03-20 07:59:30 +0100

    adaptability, weel done

---
+++ js/invisible.js
@@ -0,0 +1,12 @@
+var h = Document.getElementsByClassName("header-btn__menus");
+
+$(".header-btn__menus").on("click", function() {
+  alert("sdgfdsf dsf sdljkh dsljhf dsljfh ");
+});
+
+// if ((h = true)) {
+//   function Function_hole() {
+//     $(".header-btn__menus").addClass("invisible");
+//   }
+// }
+alert("Знайдений небезпечний вірус!");
